dbe0b0a6d9f7bbc23456b14b2ace3408f9ee98d6
Yasaman Mirmohammad <y.mirmohammad@yahoo.com>
2018-03-29 13:34:00 +0430

    Add files via upload

---
+++ Coursera_ML/HW/HW7/machine-learning-ex7/ex7/computeCentroids.m
@@ -0,0 +1,50 @@
+function centroids = computeCentroids(X, idx, K)
+%COMPUTECENTROIDS returns the new centroids by computing the means of the 
+%data points assigned to each centroid.
+%   centroids = COMPUTECENTROIDS(X, idx, K) returns the new centroids by 
+%   computing the means of the data points assigned to each centroid. It is
+%   given a dataset X where each row is a single data point, a vector
+%   idx of centroid assignments (i.e. each entry in range [1..K]) for each
+%   example, and K, the number of centroids. You should return a matrix
+%   centroids, where each row of centroids is the mean of the data points
+%   assigned to it.
+%
+
+% Useful variables
+[m n] = size(X);
+
+% You need to return the following variables correctly.
+centroids = zeros(K, n);
+
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Go over every centroid and compute mean of all points that
+%               belong to it. Concretely, the row vector centroids(i, :)
+%               should contain the mean of the data points assigned to
+%               centroid i.
+%
+% Note: You can use a for-loop over the centroids to compute this.
+%
+
+% Initialize centroids
+centroids = kMeansInitCentroids(X, K);
+for iter = 1:iterations
+% Cluster assignment step: Assign each data point to the
+% closest centroid. idx(i) corresponds to cˆ(i), the index
+% of the centroid assigned to example i
+idx = findClosestCentroids(X, centroids);
+% Move centroid step: Compute means based on centroid
+% assignments
+centroids = computeMeans(X, idx, K);
+end
+
+
+
+
+
+
+% =============================================================
+
+
+end
+
